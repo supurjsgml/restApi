87b7c39ba8117b60825ed2233267af1c1f38ff60
guney <supurjsgml1@naver.com>
2026-03-08 23:00:50 +0900

    읽기 타임아웃 30초로 변경

--- src/main/java/com/app/config/WebClientConfig.java
+++ src/main/java/com/app/config/WebClientConfig.java
@@ -24,12 +24,12 @@ public class WebClientConfig {
         //커넥션 풀 및 타임아웃 상세 설정
         HttpClient httpClient = HttpClient.create()
                 //연결 타임아웃
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000) 
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000) 
                 //응답 타임아웃
                 .responseTimeout(Duration.ofSeconds(5))
                 //읽기/쓰기 타임아웃 추가
                 .doOnConnected(conn -> conn
-                        .addHandlerLast(new ReadTimeoutHandler(5, TimeUnit.SECONDS))
+                        .addHandlerLast(new ReadTimeoutHandler(30, TimeUnit.SECONDS))
                         .addHandlerLast(new WriteTimeoutHandler(5, TimeUnit.SECONDS)));
 
         //WebClient 빌드
