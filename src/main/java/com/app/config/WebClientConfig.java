f3aa17c45638723412bcdc5d3756247c6bf5473f
guney <supurjsgml1@naver.com>
2026-08-26 22:13:36 +0900

    가난해서 그래 ;ㅅ ;

--- src/main/java/com/app/config/WebClientConfig.java
+++ src/main/java/com/app/config/WebClientConfig.java
@@ -24,13 +24,13 @@ public class WebClientConfig {
         //커넥션 풀 및 타임아웃 상세 설정
         HttpClient httpClient = HttpClient.create()
                 //연결 타임아웃
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30000) 
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 60000) 
                 //응답 타임아웃
-                .responseTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(60))
                 //읽기/쓰기 타임아웃 추가
                 .doOnConnected(conn -> conn
-                        .addHandlerLast(new ReadTimeoutHandler(30, TimeUnit.SECONDS))
-                        .addHandlerLast(new WriteTimeoutHandler(30, TimeUnit.SECONDS)));
+                        .addHandlerLast(new ReadTimeoutHandler(60, TimeUnit.SECONDS))
+                        .addHandlerLast(new WriteTimeoutHandler(60, TimeUnit.SECONDS)));
 
         //WebClient 빌드
         return builder
